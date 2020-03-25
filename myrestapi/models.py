e01d20a326272e37aefa30bb4e921f371ffd8966
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-03-25 12:09:27 +0300

    Initial commit

---
+++ myrestapi/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_description = models.CharField(max_length=255)
+    def __str__(self):
+        return self.project_title
