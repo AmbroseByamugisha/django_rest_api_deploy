29f94cf002e288b00e40bcdec82ecabee5eb6b2b
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-03-27 12:40:24 +0300

    initial

--- myapi/settings.py
+++ myapi/settings.py
@@ -140,3 +140,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #heoku-comment 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+#
