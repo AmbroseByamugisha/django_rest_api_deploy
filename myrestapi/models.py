e85dbe96b3d8bda8b8754edc9627277306c93552
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-03-27 12:31:35 +0300

    updated the models

--- myrestapi/models.py
+++ myrestapi/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    project_title = models.CharField(max_length=255)
-    project_description = models.CharField(max_length=255)
+    project_title = models.CharField(max_length=500)
+    project_description = models.CharField(max_length=500)
     def __str__(self):
         return self.project_title
