ab8e3016f1388d33fee07a1f03be21bb0f073532
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-03-29 15:27:52 +0300

    add auth privileges

--- myapi/urls.py
+++ myapi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myrestapi.urls')),
-    path('programs/', include('myprojects.urls')),
+    path('', include('myprojects.urls')),
 ]
